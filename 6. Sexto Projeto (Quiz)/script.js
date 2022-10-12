45e34850007f9c14d77eaa94f9fa1ea5422d1529
Ronald Rodrigues Pereira <57762848+RonMakaveli@users.noreply.github.com>
2022-10-12 00:07:02 -0300

    Update script.js

--- 6. Sexto Projeto (Quiz)/script.js
+++ 6. Sexto Projeto (Quiz)/script.js
@@ -4,6 +4,9 @@ let correctAnswers = 0;
 
 showQuestion();
 
+//Events
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
+
 //Funções
 function showQuestion(){
     if(questions[currentQuention]){
@@ -47,6 +50,17 @@ function optionClickEvent(e){
 
 function finishQuiz(){
     let points = Math.floor((correctAnswers / questions.length) * 100);
+
+    if(points < 30){
+        document.querySelector('.scoreText1').innerHTML = 'Tá ruim hein ?!';
+        document.querySelector('.scorePct').style.color = '#FF0000';
+    } else if(points >= 30 && points < 70){
+        document.querySelector('.scoreText1').innerHTML = 'Muito bom...';
+        document.querySelector('.scorePct').style.color = '#FFFF00';
+    } else if(points >= 70){
+        document.querySelector('.scoreText1').innerHTML = 'Parabéns !';
+        document.querySelector('.scorePct').style.color = '#0D630D';
+    }
     
     document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
     document.querySelector('.scoreText2').innerHTML = 
@@ -55,4 +69,10 @@ function finishQuiz(){
     document.querySelector('.scoreArea').style.display = 'block';
     document.querySelector('.questionArea').style.display = 'none';
     document.querySelector('.progress--bar').style.width = '100%';
+}
+
+function resetEvent(){
+    correctAnswers = 0;
+    currentQuention = 0;
+    showQuestion();
 }
