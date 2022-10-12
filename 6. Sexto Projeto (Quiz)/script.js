bd034688b2b2f87b955e8ae47b055e0d4e1236a3
Ronald Rodrigues Pereira <57762848+RonMakaveli@users.noreply.github.com>
2022-10-11 23:18:19 -0300

    Add files via upload

---
+++ 6. Sexto Projeto (Quiz)/script.js
@@ -0,0 +1,28 @@
+//Dados Iniciais
+let currentQuention = 0; //Array começa no 0.
+
+showQuestion();
+
+
+//Funções
+function showQuestion(){
+    if(questions[currentQuention]){
+        let q = questions[currentQuention];
+
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+        document.querySelector('.options').innerHTML = '';
+
+        let optionsHtml = '';
+        for(let i in q.options){
+            optionsHtml += `<div data-op="${i}" class="option">
+                <span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+    } else {
+
+    }
+}
